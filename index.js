aadd9b2f5f2c2ba93b96a323bd3734e4c2d4ea98
@hbalazs <balazs.horvath@aldi-sued.com>
2024-06-19 11:40:07 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+/*
+The solution for the mistery is hidden in this string:
+
+QSBtZWdvbGTDoXMgMyBwb250b3Qgw6lyLkEgbWVnb2xkw6FzIDMgcG9udG90IMOpci5BIG1lZ29sZMOhcyAzIHBvbnRvdCDDqXIuQSBtZWdvbGTDoXMgMyBwb250b3Qgw6lyLkEgbWVnb2xkw6FzIDMgcG9udG90IMOpci5BIG1lZ29sZMOhcyAzIHBvbnRvdCDDqXIuQSBtZWdvbGTDoXMgMyBwb250b3Qgw6lyLkEgbWVnb2xkw6FzIDMgcG9udG90IMOpci5BIG1lZ29sZMOhcyAzIHBvbnRvdCDDqXIuQSBtZWdvbGTDoXMgMyBwb250b3Qgw6lyLg==
+
+
+* Solve it and have your reward!
+* Send your code to the "ACTION TEAM"
+* You shall not use any 3rd party library for the solution
+
+*/
